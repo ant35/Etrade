6199786eb42621522b3cb48829c40b6d9429f325
ant35 <33442373+ant35@users.noreply.github.com>
2017-11-07 00:02:00 -0500

    Update cdfn.m

--- +Probability/cdfn.m
+++ +Probability/cdfn.m
@@ -1,5 +1,5 @@
 function [ p ] = cdfn( X, m, sig2 )
-%UNTITLED2 Returns cumulative distribution of X~N(m,sig2)
+%cdfn Returns cumulative distribution of X~N(m,sig2)
 %   Detailed explanation goes here
 pd = makedist('Normal',m, sig2);
 p = cdf(pd,X);
