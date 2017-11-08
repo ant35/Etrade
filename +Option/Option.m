0bb960ea7c77d5740064388466c987c0de6aab2e
Alex Torres <33442373+ant35@users.noreply.github.com>
2017-11-08 11:49:09 -0500

    Implement feature to save tree
    Will be used in future portfolio class

--- +Option/Option.m
+++ +Option/Option.m
@@ -7,13 +7,27 @@ classdef Option
         premium
         maturity
         underlying
+        underlying_tree
+        option_tree
     end
     
     methods
         function obj = Option()
             %default constructor
         end
-    end
     
+         
+        %Should only be set if the option is bought. Keeps track of
+        %which path the option has taken
+        function [obj] = set.underlying_tree(obj,tree)
+            obj.underlying_tree = tree;
+        end
+        
+        function [obj] = set.option_tree(obj,tree)
+            obj.option_tree = tree;
+        end
+    
+    end
+
 end
 
