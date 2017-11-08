898962a0780fc20b84f857f02db70cc22309da64
Alex Torres <33442373+ant35@users.noreply.github.com>
2017-11-08 11:51:07 -0500

    Beginning of load/save feature
    Going to implement portfolio class using save() and load() functions. Updated to remember approach.

--- main.m
+++ main.m
@@ -3,12 +3,13 @@ import Option.ETF.Strategy.Straddle.*;
 import Stock.grabPrices Stock.plotStock;
 warning('off','stats:dataset:ModifiedVarnames'); %supress warning
 
-opt = PutOption(258,.47,10,'APRN');
 
-initial = 258;
-S0 = 258.67;
-[v,vol,mytree,myoptiontree] = V0(opt,S0);
-iv = getImpliedVolatility(opt,initial,0.0235);
-disp(['Implied volatiliy: ' num2str(iv)]);
-[~,iv,myimptree,myimpoptiontree] = V0(opt,S0,iv);
-plotStock(opt.underlying,opt.maturity);
+opt = PutOption(258,.47,2,'SPY');
+save('portfolio.mat','opt');
+% initial = 258;
+% S0 = 258.67;
+% [v,vol,mytree,myoptiontree] = V0(opt,S0);
+% iv = getImpliedVolatility(opt,initial,0.0235);
+% disp(['Implied volatiliy: ' num2str(iv)]);
+% [~,iv,myimptree,myimpoptiontree] = V0(opt,S0,0.08);
+% plotStock(opt.underlying,opt.maturity);
